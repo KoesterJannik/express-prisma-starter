63ba9f9de46cec7b36c1ab5d2e93aa998b75a80b
Jannik Köster <koesterjannik1998@gmail.com>
2023-11-13 10:55:18 -0800

    added port to env

--- server/src/index.ts
+++ server/src/index.ts
@@ -119,6 +119,7 @@ app.get("/logout", async (req, res) => {
   return res.redirect("/login");
 });
 
-app.listen(3000, () => {
-  console.log(`Server listening on port ${3000}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
